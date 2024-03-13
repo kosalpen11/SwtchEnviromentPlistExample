0e5f3a1bd6ad6a789d3efe52a34004cd946b918d
Kosal Pen <kosal@wonderpass.asia>
2024-03-13 10:04:54 +0700

    Initial Commit

---
+++ SwtchEnviromentPlistExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwtchEnviromentPlistExample
+//
+//  Created by Kosal Pen on 13/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwtchEnviromentPlistExample/SwtchEnviromentPlistExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwtchEnviromentPlistExampleApp.swift
+//  SwtchEnviromentPlistExample
+//
+//  Created by Kosal Pen on 13/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwtchEnviromentPlistExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
